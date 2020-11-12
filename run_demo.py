7d25d5da92ef2cbfdfd50274ef372305f09d453f
821278796@qq.com <12345687>
2020-11-12 17:25:36 +0800

    1111

--- run_demo.py
+++ run_demo.py
@@ -4,12 +4,13 @@ import torch.backends.cudnn as cudnn
 import models
 import torchvision.transforms as transforms
 import flow_transforms
-from scipy.ndimage import imread
-from scipy.misc import imsave
+from imageio import imread
+from imageio import imsave
 from loss import *
 import time
 import random
 from glob import glob
+import cv2
 
 import matplotlib.pyplot as plt
 
@@ -30,38 +31,38 @@ usage:
 results will be saved at the args.output
 
 '''
-
-
+print(torch.cuda.is_available())
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 model_names = sorted(name for name in models.__dict__
                      if name.islower() and not name.startswith("__"))
 
-
 parser = argparse.ArgumentParser(description='PyTorch SPixelNet inference on a folder of imgs',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('--data_dir', metavar='DIR', default='./demo/inputs', help='path to images folder')
-parser.add_argument('--data_suffix',  default='jpg', help='suffix of the testing image')
+parser.add_argument('--data_suffix', default='jpg', help='suffix of the testing image')
 parser.add_argument('--pretrained', metavar='PTH', help='path to pre-trained model',
-                                    default= './pretrain_ckpt/SpixelNet_bsd_ckpt.tar')
-parser.add_argument('--output', metavar='DIR', default= './demo' , help='path to output folder')
+                    default='./pretrain_ckpt/SpixelNet_bsd_ckpt.tar')
+parser.add_argument('--output', metavar='DIR', default='./demo', help='path to output folder')
 
-parser.add_argument('--downsize', default=16, type=float,help='superpixel grid cell, must be same as training setting')
+parser.add_argument('--downsize', default=16, type=float, help='superpixel grid cell, must be same as training setting')
 
-parser.add_argument('-nw', '--num_threads', default=1, type=int,  help='num_threads')
+parser.add_argument('-nw', '--num_threads', default=1, type=int, help='num_threads')
 parser.add_argument('-b', '--batch-size', default=1, type=int, metavar='N', help='mini-batch size')
 
 args = parser.parse_args()
 
 random.seed(100)
+
+
 @torch.no_grad()
 def test(args, model, img_paths, save_path, idx):
-      # Data loading code
+    # Data loading code
     input_transform = transforms.Compose([
         flow_transforms.ArrayToTensor(),
-        transforms.Normalize(mean=[0,0,0], std=[255,255,255]),
-        transforms.Normalize(mean=[0.411,0.432,0.45], std=[1,1,1])
+        transforms.Normalize(mean=[0, 0, 0], std=[255, 255, 255]),
+        transforms.Normalize(mean=[0.411, 0.432, 0.45], std=[1, 1, 1])
     ])
 
     img_file = img_paths[idx]
@@ -71,7 +72,7 @@ def test(args, model, img_paths, save_path, idx):
     # may get 4 channel (alpha channel) for some format
     img_ = imread(load_path)[:, :, :3]
     H, W, _ = img_.shape
-    H_, W_  = int(np.ceil(H/16.)*16), int(np.ceil(W/16.)*16)
+    H_, W_ = int(np.ceil(H / 16.) * 16), int(np.ceil(W / 16.) * 16)
 
     # get spixel id
     n_spixl_h = int(np.floor(H_ / args.downsize))
@@ -81,12 +82,11 @@ def test(args, model, img_paths, save_path, idx):
     spix_idx_tensor_ = shift9pos(spix_values)
 
     spix_idx_tensor = np.repeat(
-      np.repeat(spix_idx_tensor_, args.downsize, axis=1), args.downsize, axis=2)
+        np.repeat(spix_idx_tensor_, args.downsize, axis=1), args.downsize, axis=2)
 
     spixeIds = torch.from_numpy(np.tile(spix_idx_tensor, (1, 1, 1, 1))).type(torch.float).cuda()
 
-    n_spixel =  int(n_spixl_h * n_spixl_w)
-
+    n_spixel = int(n_spixl_h * n_spixl_w)
 
     img = cv2.resize(img_, (W_, H_), interpolation=cv2.INTER_CUBIC)
     img1 = input_transform(img)
@@ -99,10 +99,11 @@ def test(args, model, img_paths, save_path, idx):
 
     # assign the spixel map
     curr_spixl_map = update_spixl_map(spixeIds, output)
-    ori_sz_spixel_map = F.interpolate(curr_spixl_map.type(torch.float), size=( H_,W_), mode='nearest').type(torch.int)
+    ori_sz_spixel_map = F.interpolate(curr_spixl_map.type(torch.float), size=(H_, W_), mode='nearest').type(torch.int)
 
     mean_values = torch.tensor([0.411, 0.432, 0.45], dtype=img1.cuda().unsqueeze(0).dtype).view(3, 1, 1)
-    spixel_viz, spixel_label_map = get_spixel_image((ori_img + mean_values).clamp(0, 1), ori_sz_spixel_map.squeeze(), n_spixels= n_spixel,  b_enforce_connect=True)
+    spixel_viz, spixel_label_map = get_spixel_image((ori_img + mean_values).clamp(0, 1), ori_sz_spixel_map.squeeze(),
+                                                    n_spixels=n_spixel, b_enforce_connect=True)
 
     # ************************ Save all result********************************************
     # save img, uncomment it if needed
@@ -111,7 +112,6 @@ def test(args, model, img_paths, save_path, idx):
     # spixl_save_name = os.path.join(save_path, 'img', imgId + '.jpg')
     # img_save = (ori_img + mean_values).clamp(0, 1)
     # imsave(spixl_save_name, img_save.detach().cpu().numpy().transpose(1, 2, 0))
-
 
     # save spixel viz
     if not os.path.isdir(os.path.join(save_path, 'spixel_viz')):
@@ -126,11 +126,11 @@ def test(args, model, img_paths, save_path, idx):
     #   # plus 1 to make it consistent with the toolkit format
     # np.savetxt(output_path, (spixel_label_map + 1).astype(int), fmt='%i',delimiter=",")
 
-
     if idx % 10 == 0:
-        print("processing %d"%idx)
+        print("processing %d" % idx)
 
     return toc
+
 
 def main():
     global args, save_path
@@ -153,17 +153,19 @@ def main():
 
     # create model
     network_data = torch.load(args.pretrained)
+    # network_data = torch.load(args.pretrained, map_location='cpu')
     print("=> using pre-trained model '{}'".format(network_data['arch']))
-    model = models.__dict__[network_data['arch']]( data = network_data).cuda()
+    model = models.__dict__[network_data['arch']](data=network_data).cuda()
     model.eval()
     args.arch = network_data['arch']
     cudnn.benchmark = True
 
     mean_time = 0
     for n in range(len(tst_lst)):
-      time = test(args, model, tst_lst, save_path, n)
-      mean_time += time
-    print("avg_time per img: %.3f"%(mean_time/len(tst_lst)))
+        time = test(args, model, tst_lst, save_path, n)
+        mean_time += time
+    print("avg_time per img: %.3f" % (mean_time / len(tst_lst)))
+
 
 if __name__ == '__main__':
     main()
